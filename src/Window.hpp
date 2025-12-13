85ebffb6bf170c20cdb1992815ac75efb1a4484f
DanteaExe <jorgedantex@gmail.com>
2025-12-12 21:03:38 -0600

    added buffer to see changes on text

--- src/Window.hpp
+++ src/Window.hpp
@@ -7,17 +7,23 @@ private:
     Gtk::ApplicationWindow* window = nullptr;
     Gtk::TextView* text_view = nullptr;
     Gtk::ScrolledWindow* scrolled = nullptr;
+    // Document buffer (source of truth)
+    Glib::RefPtr<Gtk::TextBuffer> buffer;
 
 public:
     void OnActivate(const Glib::RefPtr<Gtk::Application>& app)
     {
-        // Crear ventana
+        // window
         window = Gtk::make_managed<Gtk::ApplicationWindow>(app);
         window->set_title("NoteThat");
         window->set_default_size(800, 600);
 
-        // Text view
+        // Buffer (text lives here)
+        buffer = Gtk::TextBuffer::create();
+
+        // text view
         text_view = Gtk::make_managed<Gtk::TextView>();
+        text_view->set_buffer(buffer);
         text_view->set_wrap_mode(Gtk::WrapMode::WORD);
 
         // Scroll
