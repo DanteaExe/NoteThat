094f76591226d412e4be138b9462bd9e3ab514cd
DanteaExe <jorgedantex@gmail.com>
2025-12-12 20:37:49 -0600

    organize and scroll :)

--- src/Window.hpp
+++ src/Window.hpp
@@ -3,17 +3,28 @@
 
 class Window
 {
+private:
+    Gtk::ApplicationWindow* window = nullptr;
+    Gtk::TextView* text_view = nullptr;
+    Gtk::ScrolledWindow* scrolled = nullptr;
+
 public:
     void OnActivate(const Glib::RefPtr<Gtk::Application>& app)
     {
-        auto window = Gtk::make_managed<Gtk::ApplicationWindow>(app);
+        // Crear ventana
+        window = Gtk::make_managed<Gtk::ApplicationWindow>(app);
         window->set_title("NoteThat");
         window->set_default_size(800, 600);
 
-        auto text_view = Gtk::make_managed<Gtk::TextView>();
+        // Text view
+        text_view = Gtk::make_managed<Gtk::TextView>();
         text_view->set_wrap_mode(Gtk::WrapMode::WORD);
 
-        window->set_child(*text_view);
+        // Scroll
+        scrolled = Gtk::make_managed<Gtk::ScrolledWindow>();
+        scrolled->set_child(*text_view);
+
+        window->set_child(*scrolled);
         window->present();
     }
 };
